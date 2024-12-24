231fac05dcabc27e6b276ba4f87c6a41efed1139
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2024-12-24 20:13:48 +0200

    first implemntation of message bus super basic

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,5 +1,12 @@
 package bgu.spl.mics;
 
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * The {@link MessageBusImpl class is the implementation of the MessageBus interface.
  * Write your implementation here!
@@ -7,8 +14,24 @@ package bgu.spl.mics;
  * All other methods and members you add the class must be private.
  */
 public class MessageBusImpl implements MessageBus {
-	
-	@Override
+	private final ConcurrentHashMap<MicroService, BlockingQueue<Message>> queues;
+	private final ConcurrentHashMap<Class<? extends Message>, List<MicroService>> subscriptions;
+	private final ConcurrentHashMap<Class<? extends Message>, AtomicInteger> roundRobinIndices;
+	private static MessageBusImpl instance;
+
+	private MessageBusImpl() {
+		queues = new ConcurrentHashMap<>();
+		subscriptions = new ConcurrentHashMap<>();
+		roundRobinIndices = new ConcurrentHashMap<>();
+	}
+	public static MessageBusImpl getInstance() {
+		if (instance == null) {
+			instance = new MessageBusImpl();
+		}
+		return instance;
+	}
+
+    @Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
 		// TODO Auto-generated method stub
 
@@ -41,6 +64,10 @@ public class MessageBusImpl implements MessageBus {
 
 	@Override
 	public void register(MicroService m) {
+		if (!queues.containsKey(m)) {
+			BlockingQueue<Message> messageQueue = new LinkedBlockingQueue<>();
+			queues.put(m, messageQueue);
+		}
 		// TODO Auto-generated method stub
 
 	}
