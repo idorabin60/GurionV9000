ac088dc1594d7817ea11b3e2b866f4cf4d9381c3
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2024-12-31 16:59:07 +0200

    change detected object event type

--- src/main/java/bgu/spl/mics/application/services/TimeService.java
+++ src/main/java/bgu/spl/mics/application/services/TimeService.java
@@ -5,13 +5,15 @@ import bgu.spl.mics.application.messages.TerminatedBroadcast;
 import bgu.spl.mics.application.messages.TickBroadcast;
 import bgu.spl.mics.application.messages.TerminatedBroadcast;
 
+import java.util.Objects;
+
 /**
  * TimeService acts as the global timer for the system.
  * It broadcasts TickBroadcast messages at regular intervals and sends TerminateBroadcast at the end.
  */
 public class TimeService extends MicroService {
     private final int tickInterval; // Time interval (in milliseconds) between ticks
-    private final int duration; // Total duration of the simulation in ticks
+    private int duration; // Total duration of the simulation in ticks
     private int currentTick; // Keeps track of the current tick
 
     /**
@@ -33,27 +35,31 @@ public class TimeService extends MicroService {
      */
     @Override
     protected void initialize() {
-        System.out.println(getName() + " started");
+        sendBroadcast(new TickBroadcast(0));
+        this.currentTick++;
 
-        try {
-            // Main tick loop
-            while (currentTick < duration) {
-                currentTick++;
+        subscribeBroadcast(TickBroadcast.class, tickBroadcast -> {
+            if (currentTick > duration) {
+                sendBroadcast(new TerminatedBroadcast("TimeService"));
+            } else {
+                try {
+                    Thread.sleep(tickInterval * 1000);
+                    currentTick++;
+                    sendBroadcast(new TickBroadcast(currentTick * (tickInterval * 1000)));
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
 
-                // Broadcast the current tick
-                sendBroadcast(new TickBroadcast(currentTick));
-                System.out.println(getName() + " sent TickBroadcast: Tick " + currentTick);
-
-                // Sleep for the next tick
-                Thread.sleep(tickInterval);
             }
 
-            // Send TerminateBroadcast after all ticks
-            sendBroadcast(new TerminatedBroadcast("TimeService"));
+        });
+        subscribeBroadcast(TerminatedBroadcast.class, terminatedBroadcast -> {
+            if (Objects.equals(terminatedBroadcast.getSender(), "FusionSlam")){
+                sendBroadcast(new TerminatedBroadcast("TimeService"));
+            }
+        });
+        subscribeBroadcast(TerminatedBroadcast.class, terminatedBroadcast -> {
             terminate();
-        } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            System.out.println(getName() + " interrupted and terminating");
-        }
+        });
     }
 }
