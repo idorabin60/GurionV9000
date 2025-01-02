309774088edffd65bac56fa20ad29fcf16e00369
idorabin60 <102471488+idorabin60@users.noreply.github.com>
2025-01-02 10:16:06 +0200

    removing redundant locks

--- src/main/java/bgu/spl/mics/application/services/TimeService.java
+++ src/main/java/bgu/spl/mics/application/services/TimeService.java
@@ -64,7 +64,7 @@ public class TimeService extends MicroService {
         try {
             System.out.println("waiting for services to be inited");
             SystemServicesCountDownLatch.getInstance().getCountDownLatch().await();
-            Thread.sleep(1000); // Allow other services to settle
+            Thread.sleep(7000); // Allow other services to settle
             sendBroadcast(new TickBroadcast(currentTick));
 
         } catch (Exception e) {
